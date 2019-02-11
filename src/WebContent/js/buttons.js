819a27adda1de6eea356edd2b39602c28cd376d5
Jordan Sinclair <joey.sinclo@gmail.com>
2019-02-11 06:17:58 -0600

    Reconfigured project for use of spring boot and thymeleaf

--- src/WebContent/js/buttons.js
+++
@@ -1,7 +0,0 @@
-function navigateToRobotTeamPage(){
-    window.location = "../pages/robotTeam.html"
-}
-
-function navigateToRobotTeammatesPage(){
-    window.location = "../pages/robotTeammates.html"
-}
