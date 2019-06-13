402d93f52bb9c16b92cbe6c4742fd11227914ac0
Jordan Sinclair <joey.sinclo@gmail.com>
2019-06-13 04:59:09 -0500

    Updated schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,10 @@
 create table if not exists SportsTeam(
     id int(10) unsigned not null auto_increment primary key,
     teamname varchar(60) not null,
-    ownerfirstname varchar(30),
-    ownerlastname varchar(30),
+    ownerfirstname varchar(30) not null,
+    ownerlastname varchar(30) not null,
     createDate datetime default current_timestamp,
-    lastUpdatedDate datetime default current_timestamp on update current_timestamp
+    lastUpdatedDate datetime default current_timestamp on update current_timestamp,
+    unique key unique_teamname(teamname)
 );
 
