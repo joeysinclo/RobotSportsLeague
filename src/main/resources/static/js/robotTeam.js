97fe65a46360f848244f60b7e3c4d28777ca0a66
Jordan Sinclair <joey.sinclo@gmail.com>
2019-06-22 09:38:48 -0500

    Front-end work.  Adding in ability to edit

--- src/main/resources/static/js/robotTeam.js
+++ src/main/resources/static/js/robotTeam.js
@@ -1,29 +1,55 @@
 // Get the modal
-var inputModal = document.getElementById('createTeamInput');
+var createInputModal = document.getElementById('createTeamInput');
+var editInputModal = document.getElementById('editTeamInput');
 var confirmModal = document.getElementById('createTeamConfirmation');
 
-// Get the button that opens the modal
+// Get the buttons that opens the modal
 var createTeamBtn = document.getElementById("createTeamButton");
-var submitInputBtn = document.getElementById("submitButton");
+var editTeamBtn = document.getElementById("editTeamButton");
+
+// Get the buttons that saves input provided by user
+var createInputBtn = document.getElementById("createInputButton");
+var editInputBtn = document.getElementById("editInputButton");
 
 // Get the element that closes the modal
-var inputCloseBtn = document.getElementById("close_input_button");
+var createInputCloseBtn = document.getElementById("close_create_team_input_button");
+var editInputCloseBtn = document.getElementById("close_edit_team_input_button");
 var confirmCloseBtn = document.getElementById("close_confirmation_button");
 
 
-// When the user clicks the button, open the modal
+// When the user clicks the "create team" button, open the 'create team' input modal
 createTeamBtn.onclick = function() {
-  inputModal.style.display = "block";
+  createInputModal.style.display = "block";
 }
 
-// When the user clicks this button, display 'confirmation modal'
-submitInputBtn.onclick = function() {
+// When the user clicks the edit button, open the 'edit team' input modal
+editTeamBtn.onclick = function() {
+  editInputModal.style.display = "block";
+}
 
-  var teamNameInput = document.getElementById("teamNameInput").value;
-  var firstNameInput = document.getElementById("firstNameInput").value;
-  var lastNameInput = document.getElementById("lastNameInput").value;
+// When the user clicks 2nd "create team" button, display 'confirmation modal'
+createInputBtn.onclick = function() {
 
-  inputModal.style.display = "none"
+  var teamNameInput = document.getElementById("createTeamNameInput").value;
+  var firstNameInput = document.getElementById("createFirstNameInput").value;
+  var lastNameInput = document.getElementById("createLastNameInput").value;
+
+  createInputModal.style.display = "none";
+  confirmModal.style.display = "block";
+
+  document.getElementById("teamNameValue").innerHTML = teamNameInput;
+  document.getElementById("firstNameValue").innerHTML = firstNameInput;
+  document.getElementById("lastNameValue").innerHTML = lastNameInput;
+}
+
+// When the user clicks 2nd "edit team" button, display 'confirmation modal'
+editInputBtn.onclick = function() {
+
+//  var teamNameInput = document.getElementById("editTeamNameInput").value;
+//  var firstNameInput = document.getElementById"editFirstNameInput").value;
+//  var lastNameInput = document.getElementById("editLastNameInput").value;
+
+  editInputModal.style.display = "none";
   confirmModal.style.display = "block";
 
   document.getElementById("teamNameValue").innerHTML = teamNameInput;
@@ -32,8 +58,11 @@ submitInputBtn.onclick = function() {
 }
 
 // When the user clicks on (x), close the modal
-inputCloseBtn.onclick = function() {
-  inputModal.style.display = "none";
+createInputCloseBtn.onclick = function() {
+  createInputModal.style.display = "none";
+}
+editInputCloseBtn.onclick = function() {
+  editInputModal.style.display = "none";
 }
 confirmCloseBtn.onclick = function() {
   confirmModal.style.display = "none";
